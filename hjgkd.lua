b5cb50346a23e21b86dad5a9b796f4d94008ee08
queytoe2 <queytoe2@gmail.com>
2024-11-14 20:24:25 -0600

    Create hjgkd.lua

---
+++ hjgkd.lua
@@ -0,0 +1,58 @@
+function CheckAllParts(char)
+    for _,v in pairs(Tables.Settings.BodyParts) do
+        if not char:FindFirstChild(v) then
+            char:WaitForChild(v,math.huge)
+        end
+    end
+    return true
+end
+
+for i,v in pairs(game.Players:GetPlayers()) do
+    task.wait(1)
+    pcall(function()
+        for i,v in pairs(v.Backpack:GetChildren()) do
+            if v.Name == "Shuriken" and v:FindFirstChild("HitEvent") then
+                v:FindFirstChild("HitEvent"):FireServer(Vector2.new())
+            end
+        end
+        for i,v in pairs(v.Character:GetChildren()) do
+            if v.Name == "Shuriken" and v:FindFirstChild("HitEvent") then
+                v:FindFirstChild("HitEvent"):FireServer(Vector2.new())
+            end
+        end
+    end)
+    v.CharacterAdded:Connect(function(c)
+        CheckAllParts(c)
+        task.wait(1)
+        pcall(function()
+            for i,v in pairs(v.Backpack:GetChildren()) do
+                if v.Name == "Shuriken" and v:FindFirstChild("HitEvent") then
+                    v:FindFirstChild("HitEvent"):FireServer(Vector2.new())
+                end
+            end
+            for i,v in pairs(v.Character:GetChildren()) do
+                if v.Name == "Shuriken" and v:FindFirstChild("HitEvent") then
+                    v:FindFirstChild("HitEvent"):FireServer(Vector2.new())
+                end
+            end
+        end)
+    end)
+end
+
+game.Players.PlayerAdded:Connect(function(p)
+    p.CharacterAdded:Connect(function(c)
+    CheckAllParts(c)
+    task.wait(1)
+    pcall(function()
+        for i,v in pairs(v.Backpack:GetChildren()) do
+            if v.Name == "Shuriken" and v:FindFirstChild("HitEvent") then
+                v:FindFirstChild("HitEvent"):FireServer(Vector2.new())
+            end
+        end
+        for i,v in pairs(v.Character:GetChildren()) do
+            if v.Name == "Shuriken" and v:FindFirstChild("HitEvent") then
+                v:FindFirstChild("HitEvent"):FireServer(Vector2.new())
+            end
+        end
+    end)
+end)
